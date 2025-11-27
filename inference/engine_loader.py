74883dc3c23a52113162ae3f96b098fb72bf1e8a
Youngchul Jung <youngchul.jung@gamil.com>
2025-11-27 15:14:09 +0900

    init

--- inference/engine_loader.py
+++ inference/engine_loader.py
@@ -10,15 +10,12 @@ class TRTInferenceEngine:
     def __init__(self, engine_path):
         self.logger = trt.Logger(trt.Logger.INFO)
 
-        # 엔진 로드
         with open(engine_path, "rb") as f:
             runtime = trt.Runtime(self.logger)
             self.engine = runtime.deserialize_cuda_engine(f.read())
 
-        # 실행 컨텍스트
         self.context = self.engine.create_execution_context()
 
-        # 바인딩 정보 읽기
         self.input_idx = self.engine.get_binding_index("input")
         self.output_idx = self.engine.get_binding_index("output")
 
@@ -28,18 +25,22 @@ class TRTInferenceEngine:
         self.input_size = int(np.prod(input_shape))
         self.output_size = int(np.prod(output_shape))
 
-        # GPU 메모리 할당
         self.d_input = cuda.mem_alloc(self.input_size * np.float32().nbytes)
         self.d_output = cuda.mem_alloc(self.output_size * np.float32().nbytes)
 
-        # host buffer
         self.h_output = np.empty(self.output_size, dtype=np.float32)
 
-        # 스트림
         self.stream = cuda.Stream()
 
     def infer(self, input_np):
-        # input_np: (1, 3, 66, 200) float32
+        """
+        input_np: (1, 3, 66, 200) float32
+        """
+        # ★ dtype 및 contiguous 보장
+        input_np = np.asarray(input_np, dtype=np.float32)
+        if not input_np.flags["C_CONTIGUOUS"]:
+            input_np = np.ascontiguousarray(input_np)
+
         cuda.memcpy_htod_async(self.d_input, input_np, self.stream)
 
         self.context.execute_async_v2(
